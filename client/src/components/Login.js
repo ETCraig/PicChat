27987a45e36470618dd58306ce6f1d7fffd9b3d4
unknown <devethancraig@gmail.com>
2019-02-15 12:27:37 -0700

    Setup Login && Register Frontend Calls

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from '../actions/AuthActions';
 import PropTypes from 'prop-types';
 
 class Login extends Component {
@@ -11,14 +12,71 @@ class Login extends Component {
             password: '',
             errors: {}
         }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-    render() {
-        return(
-            <div>
+    componentDidMount() {
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push('/Feed');
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push('/Feed');
+        }
+        if (nextProps.errors) {
+            const { errors } = nextProps;
+            this.setState({ errors });
+        }
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onSubmit(e) {
+        let { email, password } = this.state;
+        e.preventDefault();
+        const userData = {
+            email,
+            password
+        }
+        this.props.loginUser(userData);
+    }
 
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <form noValidate onSubmit={this.onSubmit}>
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='Email'
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        onChange={this.onChange}
+                    />
+                    <button type='submit'>Login</button>
+                </form>
             </div>
         );
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+    return {
+        auth: state.auth,
+        errors: state.errors
+    }
+}
+
+export default connect(mapStateToProps, { loginUser })(Login);
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { registerUser } from '../actions/AuthActions';
+import { withRouter } from 'react-router-dom';
 
 class Register extends Component {
     constructor() {
@@ -15,14 +17,106 @@ class Register extends Component {
             password2: '',
             errors: {}
         }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    componentDidMount() {
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push('/Feed');
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            let { errors } = nextProps;
+            this.setState({ errors });
+        }
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onSubmit(e) {
+        let {
+            userName,
+            firstName,
+            lastName,
+            email,
+            password,
+            password2
+        } = this.state;
+        e.preventDefault();
+        const newUser = {
+            userName,
+            firstName,
+            lastName,
+            email,
+            password,
+            password2
+        }
+        this.props.registerUser(newUser, this.props.history);
     }
     render() {
-        return(
+        return (
             <div>
-
+                <h1>Create Your New Account</h1>
+                <form noValidate onSubmit={this.onSubmit}>
+                    <input
+                        name='userName'
+                        type='name'
+                        placeholder='userName'
+                        value={this.state.name}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='firstName'
+                        type='name'
+                        placeholder='firstName'
+                        value={this.state.name}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='lastName'
+                        type='name'
+                        placeholder='lastName'
+                        value={this.state.name}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='Email'
+                        value={this.state.email}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        value={this.state.password}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        name='password2'
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={this.state.password2}
+                        onChange={this.onChange}
+                    />
+                    <button type='submit'>Create Account</button>
+                </form>
             </div>
         );
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
